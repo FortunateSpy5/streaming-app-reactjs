2fb368b2428f1aa787103342fb7150344bef05c7
Soumotanu Mazumdar <msoumotanu@gmail.com>
2020-10-17 17:08:04 +0530

    Modal Added (React Portals)

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -15,9 +15,9 @@ class StreamList extends React.Component {
                   <Link className="ui primary basic button" to={`/streams/edit/${stream.id}`}>
                       Edit
                   </Link>
-                  <button className="ui negative basic button">
+                  <Link className="ui negative basic button" to={`/streams/delete/${stream.id}`}>
                       Delete
-                  </button>
+                  </Link>
               </div>
             );
         }
