01b0c8db24ceb5fa51c9e6490f55f4cc58aa1881
Soumotanu Mazumdar <msoumotanu@gmail.com>
2020-10-17 13:29:38 +0530

    StreamList Rendering + Edit/Delete buttons

--- streams/client/src/components/Header.js
+++ streams/client/src/components/Header.js
@@ -4,7 +4,7 @@ import GoogleAuth from "./GoogleAuth";
 
 const Header = () => {
     return (
-        <div className="ui secondary pointing menu">
+        <div className="ui secondary pointing menu" style={{padding: '0.4rem 0 0.3rem 0'}}>
             <Link to="/" className="item">
                 Streamer
             </Link>
